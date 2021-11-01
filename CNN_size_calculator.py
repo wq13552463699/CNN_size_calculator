099b8ce0fd15a08dd00bf4a43b02b67dce434647
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2021-11-01 18:07:14 +0000

    Add files via upload

---
+++ CNN_size_calculator.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 20:43:29 2021
+
+@author: 14488
+"""
+
+W = 66
+k = 4
+S = 2
+P = 0
+
+
+
+N = ((W - k + (2*P))/S)+1
+print(N)
+
+#%%
+
+W = 66
+k = 4
+S = 2
+P = 0
+
+N=((W-1)*S)+k-(2*P) 
+print(N)
